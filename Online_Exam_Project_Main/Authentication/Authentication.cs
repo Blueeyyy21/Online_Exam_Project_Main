652e10c55ab4986936dc919027627cbf41b87750
atharva <pawaratharva878@gmail.com>
2023-05-14 14:56:41 +0530

    Add project files.

---
+++ Online_Exam_Project_Main/Authentication/Authentication.cs
@@ -0,0 +1,25 @@
+﻿//using System.Web.Mvc;
+//using System.Web.Routing;
+
+//namespace Online_Exam_Project_Main.Authentication
+//{
+//    internal class Authentication : ActionFilterAttribute
+//    {
+//        public override void OnActionExecuting(ActionExecutingContext context)
+//        {
+//            if(context.HttpContext.Session.GetString("admin_name") == null)
+//            {
+//                context.Result = new RedirectToRouteResult(new RouteValueDictionary
+//                {
+//                    {
+//                        "Controller", "Admin"
+//                    },
+//                    {
+//                        "Action" , "Index"
+//                    }
+
+//                });
+//            }
+//        }
+//    }
+//}
